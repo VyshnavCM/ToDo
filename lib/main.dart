e6a0d11887f78b7b3c55a903d6311996db743b7a
Vyshnav_Vijayan <vvijayan854@gmail.com>
2024-07-30 22:55:09 +0530

    App's theme incorporated

--- lib/main.dart
+++ lib/main.dart
@@ -13,78 +13,63 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
+        textTheme: const TextTheme(
+            displayLarge: TextStyle(
+              color: Colors.black,
+              fontSize: 45,
+              fontWeight: FontWeight.bold,
+            ),
+            titleMedium: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+            displayMedium: TextStyle(
+              color: Colors.white,
+              fontSize: 21,
+            ),
+            displaySmall: TextStyle(
+              color: Color.fromARGB(255, 234, 234, 234),
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+            headlineSmall: TextStyle(
+              color: Colors.grey,
+              fontSize: 16,
+            ),
+            headlineMedium: TextStyle(
+              color: Colors.grey,
+              fontSize: 17,
+            ),
+            titleSmall: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+            ),
+            titleLarge: TextStyle(
+                color: Colors.black,
+              fontSize: 40,
+              fontWeight: FontWeight.w300,
+            )),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Test(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+
+
+class Test extends StatefulWidget {
+  const Test({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Test> createState() => _TestState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class _TestState extends State<Test> {
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return const Placeholder();
   }
 }
